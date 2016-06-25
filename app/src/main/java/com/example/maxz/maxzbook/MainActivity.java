bf5dc8045ab8aa6c784b54f93e78e95ad08f89c6
maxz <maxz_drum@icloud.com>
2016-06-25 11:21:57 +0700

    Inten ปุ่ม SignUP แล้ว

--- app/src/main/java/com/example/maxz/maxzbook/MainActivity.java
+++ app/src/main/java/com/example/maxz/maxzbook/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.maxz.maxzbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//onCreate
+
+
+
+    }//onCreate main method
+    public void clickSignUP (View view){
+
+        startActivity(new Intent(MainActivity.this,SignUPActivity.class));
+
+    }
+
 }//main class
