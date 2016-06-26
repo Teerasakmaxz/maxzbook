27c1ceea61f3fca34caffa494a46e6aaea6fd9c4
maxz <maxz_drum@icloud.com>
2016-06-26 10:43:12 +0700

    Call server

--- app/src/main/java/com/example/maxz/maxzbook/SignUPActivity.java
+++ app/src/main/java/com/example/maxz/maxzbook/SignUPActivity.java
@@ -5,11 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUPActivity extends AppCompatActivity {
 
     //ประกาศตัวแปร
     private EditText userEditText, passEditText, nameEditText, addressEditText;
     private String userString, passString, nameString, addressString;
+    private static final String URLPHP = "http://swiftcodingthai.com/25JUN/add_user_maxz.php";
+    private static final String money = "500";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +64,34 @@ public class SignUPActivity extends AppCompatActivity {
     }//click signUP
 
     private void uploadUserToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("User", userString)
+                .add("Password", passString)
+                .add("Name", nameString)
+                .add("Address", addressString)
+                .add("Money", money)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(URLPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+             finish();
+            }
+        });
+
+
+
+
 
     }
 
